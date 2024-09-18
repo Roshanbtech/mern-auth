860509de25ddf86f28e1efb583517b3d59ef387d
Roshanbtech <roshanreji987@gmail.com>
2024-09-18 16:09:54 +0530

    header styles in jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,23 +1,45 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className='bg-slate-200'>
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-5'>
-            <Link to='/'>
-            <h1 className='font-bold'><span className='text-red-500'>R-Auth</span></h1>
+    <div className="bg-gradient-to-r from-black to-gray-900 shadow-lg">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-5">
+        <Link to="/">
+          <h1 className="text-3xl font-bold text-blue-400 tracking-wide">
+            R-Auth
+          </h1>
+        </Link>
+        <ul className="flex gap-8 text-lg">
+          <li>
+            <Link to="/" className="text-gray-300 hover:text-blue-400 transition duration-300">
+              Home
             </Link>
-            <ul className='flex gap-4'>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/about'>About</Link></li>
-                <li><Link to='/signin'>SignIn</Link></li>
-                <li><Link to='/signup'>SignUp</Link></li>
-                <li><Link to='/profile'>Profile</Link></li>
-            </ul>
-        </div>
+          </li>
+          <li>
+            <Link to="/about" className="text-gray-300 hover:text-blue-400 transition duration-300">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/signin" className="text-gray-300 hover:text-blue-400 transition duration-300">
+              SignIn
+            </Link>
+          </li>
+          <li>
+            <Link to="/signup" className="text-gray-300 hover:text-blue-400 transition duration-300">
+              SignUp
+            </Link>
+          </li>
+          <li>
+            <Link to="/profile" className="text-gray-300 hover:text-blue-400 transition duration-300">
+              Profile
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
