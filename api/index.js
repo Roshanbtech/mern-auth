ff076f48ecc6dfdbe340909283df2836b68f2c41
Roshanbtech <roshanreji987@gmail.com>
2024-09-19 11:36:49 +0530

    complete cors setup and signup page functionality

--- api/index.js
+++ api/index.js
@@ -1,33 +1,33 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import userRoutes from "./routes/userRoute.js";
 import authRoutes from "./routes/authRoute.js";
+
 dotenv.config();
 
 mongoose
-        .connect(process.env.MONGO)
-        .then(() => 
-            console.log("Connected to MongoDB")
-    )
+  .connect(process.env.MONGO)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB:", err));
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.listen(3000, () => 
-    console.log("Server is running on port 3000.")
-);
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 
-app.use('/api/user',userRoutes);
-app.use('/api/auth',authRoutes);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
 
-app.use((err,req,res,next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-    res.status(statusCode).json({
-        success: false,
-        message,
-        statusCode,
-    })
-})
+app.listen(4000, () => console.log("Server is running on port 4000."));
