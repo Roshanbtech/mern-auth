62def24251b7dfbe5342aa515b06414d0218da76
Roshanbtech <roshanreji987@gmail.com>
2024-09-19 17:21:54 +0530

    create signin api route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,13 +1,18 @@
 // import express from "express";
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
-
+import errorHandler from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 const signUp = async (req, res, next) => {
   try {
     const { userName, userEmail, userPassword } = req.body;
     const hashedPassword = await bcryptjs.hash(userPassword, 10);
-    const newUser = new User({ userName, userEmail, userPassword:hashedPassword });
+    const newUser = new User({
+      userName,
+      userEmail,
+      userPassword: hashedPassword,
+    });
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
@@ -15,4 +20,26 @@ const signUp = async (req, res, next) => {
   }
 };
 
-export { signUp };
+const signIn = async (req, res, next) => {
+  const { userEmail, userPassword } = req.body;
+  try {
+    const validUser = await User.findOne({ userEmail });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = await bcryptjs.compare(
+      userPassword,
+      validUser.userPassword
+    );
+    if (!validPassword) return next(errorHandler(401, "wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { userPassword: hashedPassword, ...others } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json({ others });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { signUp, signIn };
--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -3,5 +3,6 @@ import { signUp } from "../controllers/authController.js";
 const router = express.Router();
 
 router.post('/signup',signUp)
+router.post('/signin',signIn)
 
 export default router
