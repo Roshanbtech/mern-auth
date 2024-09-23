f2d7b7350c5b122665a1d4dfdc1b852ee985ee2c
Roshanbtech <roshanreji987@gmail.com>
2024-09-24 00:13:12 +0530

    update header component and make profile private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home'
-import About from './pages/About'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Profile from './pages/Profile'
-import Header from './components/Header'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
@@ -17,10 +18,12 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <div className="bg-gradient-to-r from-black to-gray-900 shadow-lg fixed top-0 left-0 right-0 z-10">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-5">
@@ -12,28 +15,35 @@ const Header = () => {
         </Link>
         <ul className="flex gap-8 text-lg">
           <li>
-            <Link to="/" className="text-gray-300 hover:text-blue-400 transition duration-300">
+            <Link
+              to="/"
+              className="text-gray-300 hover:text-blue-400 transition duration-300"
+            >
               Home
             </Link>
           </li>
           <li>
-            <Link to="/about" className="text-gray-300 hover:text-blue-400 transition duration-300">
+            <Link
+              to="/about"
+              className="text-gray-300 hover:text-blue-400 transition duration-300"
+            >
               About
             </Link>
           </li>
           <li>
-            <Link to="/signin" className="text-gray-300 hover:text-blue-400 transition duration-300">
-              SignIn
-            </Link>
-          </li>
-          <li>
-            <Link to="/signup" className="text-gray-300 hover:text-blue-400 transition duration-300">
-              SignUp
-            </Link>
-          </li>
-          <li>
-            <Link to="/profile" className="text-gray-300 hover:text-blue-400 transition duration-300">
-              Profile
+            <Link
+              to={currentUser ? "/profile" : "/signin"}
+              className="text-gray-300 hover:text-blue-400 transition duration-300 flex items-center"
+            >
+              {currentUser ? (
+                <img
+                  src={currentUser.userProfilePic}
+                  alt="Profile"
+                  className="w-8 h-8 rounded-full object-cover"
+                />
+              ) : (
+                <span>Sign In</span>
+              )}
             </Link>
           </li>
         </ul>
